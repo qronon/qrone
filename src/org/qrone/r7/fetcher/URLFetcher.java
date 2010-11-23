31cdabd00efa4c5e19dfddc841b517e217668d13
qronon <qronon@qronon-i7>
2010-11-23 22:18:32 +0900

    URLFetcher post support.

--- src/org/qrone/r7/fetcher/URLFetcher.java
+++ src/org/qrone/r7/fetcher/URLFetcher.java
@@ -1,7 +1,9 @@
 package org.qrone.r7.fetcher;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public interface URLFetcher {
-	public InputStream fetch(String url);
+	public InputStream fetch(String url) throws IOException;
+	public InputStream fetch(String url, byte[] body) throws IOException;
 }
